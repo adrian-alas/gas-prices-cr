1609e38a8c00e299f333be6c3d4345738b80bdef
adrian-alas <a.alas.hdz@gmail.com>
2021-07-26 23:36:09 -0600

    API request

--- components/Index.js
+++ components/Index.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
-import {StyleSheet, Text, View, Keyboard, TouchableWithoutFeedback, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, Keyboard, TouchableWithoutFeedback, Dimensions, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {Input, Button } from 'react-native-elements';
+import RNPickerSelect from 'react-native-picker-select';
 
 class Index extends Component {
+
+    request = (amount, type) => {
+        fetch('https://api.recope.go.cr/ventas/precio/consumidor',{
+            method: 'GET',
+            header: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json' 
+            }
+        }).then((response) => response.json()).then((responseJson) =>{
+
+            responseJson.forEach(price => {
+
+                const {nomprod, preciototal} = price;
+                
+                if(nomprod == type){
+                    Alert.alert(
+                        type,
+                        `Monto: ₡${amount}, Litros: ${(amount / preciototal).toFixed(3)}`,
+                        [{ text: "Aceptar"}],
+                        {cancelable: false}
+                    );
+                }
+
+            });
+
+        }).catch((error) =>{
+            alert(error);
+        });
+    }
 
    render() {
         return (
@@ -12,9 +42,19 @@ class Index extends Component {
                     <Input
                         placeholder='Monto a consultar (colones)'
                         keyboardType="numeric"
+                        onChangeText={(amount) => this.setState({amount})}
+                    />
+                    <RNPickerSelect
+                        onValueChange={(type) => this.setState({type})}
+                        items={[
+                            {label: 'Gasolina super (superior)', value: 'GASOLINA SUPER ( SUPERIOR )'},
+                            {label: 'Gasolina plus 91 (regular)', value: 'GASOLINA PLUS 91 ( REGULAR )'},
+                            {label: 'Diesel', value: 'DIESEL 50'}
+                        ]}
                     />
                     <Button
-                        title="Consultar"                         
+                        title="Consultar"   
+                        onPress = {() => this.request(this.state.amount, this.state.type)}                      
                     />
                 </View>
             </TouchableWithoutFeedback>
